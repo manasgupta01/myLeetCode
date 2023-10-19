a80289c95a20a68c868a7a68f0b02dbb0ba0bf68
manas gupta <98965385+manasgupta01@users.noreply.github.com>
2023-10-19 16:35:13 +0530

    Time: 3 ms (47.11%) | Memory: 6.6 MB (39.46%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        int i;
+        vector<char>s1,t1;
+        for(i=0;i<s.length();i++){
+            if(s[i]!='#'){
+                s1.push_back(s[i]);
+            }
+            else{
+                if(!s1.empty())
+                s1.pop_back();
+            }
+             
+        }
+        for(i=0;i<t.length();i++){
+            if(t[i]!='#'){
+                t1.push_back(t[i]);
+            }
+            else{
+                if(!t1.empty())
+                t1.pop_back();
+            }
+             
+        }
+         for(int i=0;i<s1.size();i++){
+                cout<<s1[i];
+            }
+            for(int i=0;i<t1.size();i++){
+                cout<<t1[i];
+            }
+        if(s1==t1){
+           
+            return true;
+        }
+        return false;
+
+        
+    }
+};
