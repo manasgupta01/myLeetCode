8df50ab9110e52d22aaa0a076374b631212f3d9c
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2023-12-28 21:25:22 +0530

    Time: 310 ms (5.41%) | Memory: 95.8 MB (49.56%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     int minCost(string c, vector<int>& nt) {
         int res = 0;
-        // int k = 0;
-        // int sum = accumulate(nt.begin(),nt.end(),0);
         for(int i=1;i<c.length();i++){
-        
             if(c[i-1]==c[i]){
                 if(nt[i-1]>nt[i]){
                     res+=nt[i];
                     swap(nt[i],nt[i-1]);
-                    //nt[i]=INT_MAX;
                 }
                 else{
                     res+=nt[i-1];
-                    //nt[i-1]=INT_MAX;
                 }
             }
         }
