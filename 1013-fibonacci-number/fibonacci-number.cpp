96b1614c1052fbddb87a7742445b50a82f81f416
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-02-05 11:36:54 +0530

    Time: 2 ms (77.11%) | Memory: 7.1 MB (10.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
+        
+        if(n==0 || n== 1){
+            return n;
+        }
         vector<int>k;
         k.push_back(0);
         k.push_back(1);
-        if(n==0 || n== 1){
-            return k[n];
-        }
         for(int i=2;i<=n;i++){
             k.push_back(k[i-1]+k[i-2]);
         }
