11bd723ca8a8fff0194c43dad2b5ace53f3ccb7a
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-02-05 11:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (31.97%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        if(n%2!=0){
-            return false;
-        }
-        return true;
+        return (n%2!=0)?false:true;
     }
 };
