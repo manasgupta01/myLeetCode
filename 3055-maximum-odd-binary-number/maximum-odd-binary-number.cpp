d4b45ccf271591f41bf97f3f899acba5fe215c87
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-03-01 15:50:10 +0530

    Time: 7 ms (31.42%) | Memory: 8.4 MB (49.35%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-     int last = s.length()-1;
-     bool b = false;
-    
-     if(s[last]=='1'){
-         
-         b=true;
-         
-     }
-     if(b==false){
-         cout<<"hi";
-        for(int i=s.length()-1;i>=0;i--){
-          //  cout<<s[i]<< " " << s[last]<< " \n";
-            if(s[i]=='1'){
-                cout<<"c";
-                swap(s[i],s[last]);
-                break;
-            }
-        } 
-     }
-     {
-         int l = 0;
-         for(int i=1;i<s.length()-1 && l<s.length()-1;i++){
-             cout<<s[i]<< " " << s[l]<< " \n";
-             if(s[l]=='1'){
-                 cout<<"a";
-                 l++;
-                 continue;
-             }
-             else{
-                 if(s[l]=='0' && s[i]=='1'){
-                     cout<<"b";
-                     swap(s[i],s[l]);
-                     l++;
-                 }
-             }
-         }
-     }
-     return s;
+        int ones = count(s.begin(), s.end(), '1');
+        int sz = s.size();
+   
+        string res = "";
 
+        int nonOnes = sz - ones;
+        ones--;
+
+        
+        while(ones-->0){
+            res += "1";
+        }
+        while(nonOnes-->0){
+            res += "0";
+        }
+        res+="1";
+      //  reverse(res.begin(), res.end());
+        return res;
     }
 };
