cb9bc80919eabaf3efa7d32565d7894748c39e6e
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-06-13 18:26:14 +0530

    Time: 3 ms (91.43%) | Memory: 21 MB (52.06%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -4,9 +4,14 @@ public:
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
         int sum = 0;
+
         for(int i=0;i<seats.size();i++){
+        
             sum+=(abs(seats[i]-students[i]));
+        
         }
+
+
         return sum;
     }
 };
