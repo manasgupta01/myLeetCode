c81de03b824a25509302de393c30ad488f607f1c
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-07-26 10:40:15 +0530

    Time: 722 ms (10.25%) | Memory: 284.5 MB (19.06%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -2,38 +2,30 @@ class Solution {
 public:
 
     void mergeit(vector<int> & nums, int p , int q, int r){
-        int n1 = q - p + 1;
-        int n2 = r - q;
-        vector<int> v1(n1);
-        vector<int> v2(n2);
-        for(int i=0;i<n1;i++){
-            v1[i]=nums[p+i];
-        }
-        for(int i=0;i<n2;i++){
-            v2[i] = nums[q+i+1];
-        }
-        int a = 0, b = 0, k = p;
-        while(a<n1 && b<n2){
-            if(v1[a]<=v2[b]){
-                nums[k]=v1[a];
-                k++;
+        
+        vector<int> v1;
+        int a = p, b = q+1;
+        while( a <= q && b <= r){
+            if(nums[a]<nums[b]){
+                v1.push_back(nums[a]);
                 a++;
             }
             else{
-                nums[k]=v2[b];
-                k++;
+                v1.push_back(nums[b]);
                 b++;
             }
         }
-        while(a<n1){
-              nums[k]=v1[a];
-                k++;
+        while(a<=q){
+                v1.push_back(nums[a]);
                 a++;
         }
-        while(b<n2){
-              nums[k]=v2[b];
-                k++;
+        while(b<=r){
+              v1.push_back(nums[b]);
                 b++;
+        }
+        a = 0;
+        while(p <= r){
+            nums[p++] = v1[a++];
         }
     }
 
