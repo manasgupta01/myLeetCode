6523c5c0d78e0a4374639a63fe949350c090c741
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-08-13 00:15:30 +0530

    Time: 2 ms (42.17%) | Memory: 9.9 MB (8.59%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    int memo(vector<int> &dp, int ind, vector<int>& nums) {
-        if (ind == 0) return nums[0];
-        if (ind < 0) return 0;
-        if (dp[ind] != -1) return dp[ind];
-        int pick = nums[ind] + memo(dp, ind - 2, nums);
-        int notpick = memo(dp, ind - 1, nums);
-        return dp[ind] = max(pick, notpick);
+     int tabulate(vector<int> &dp,int ind,vector<int> nums)
+    {
+        int n = nums.size();
+        dp[0] = nums[0];
+        if(n > 1)
+        {
+            dp[1] = nums[1];
+        }
+        int pick= INT_MIN;
+        int notpick =INT_MIN;
+        for(int i=1;i<n;i++)
+        {
+            pick = nums[i];
+            if(i>1) pick += dp[i-2];
+            notpick = dp[i-1];
+            dp[i] = max(pick,notpick);
+        }
+        return dp[n-1];
     }
 
       int rob(vector<int>& nums) {
@@ -21,7 +32,7 @@ public:
             arr.push_back(nums[i]);
         }
         nums.pop_back();
-        return max(memo(dp1,n-2,arr), memo(dp,n-2,nums));
+        return max(tabulate(dp1,n-2,arr), tabulate(dp,n-2,nums));
 
 
     }
