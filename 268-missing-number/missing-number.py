ce5ae1f67f0140b5b6711392f5e8707a5c59ac5f
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-09-13 11:29:48 +0530

    Time: 116 ms (37.28%) | Memory: 17.8 MB (51.63%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        totalSum = (n * (n + 1)) / 2 # sum of 'n' natural numbers
+        totalSum = (n * (n + 1)) / 2 
         currSum = 0
         for i in range(len(nums)):
             currSum += nums[i]
