371c8d42bc4881ca1c48d1cf71d52e3525e0d465
MANAS GUPTA <98965385+manasgupta01@users.noreply.github.com>
2024-10-17 14:27:43 +0530

    Time: 3 ms (38.18%) | Memory: 7.4 MB (98.12%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        int t = num;
-        string s = to_string(t);
+        string s = to_string(num),mx = s;
         bool b = false;
-        string mx = s;
+        int j;
         for(int i=0;i<s.length()-1;i++){
-            int j=i+1;
+            j=i+1;
             while(j<s.length()){
                 if(s[i]<s[j]){
                     swap(s[i],s[j]);
